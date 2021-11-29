9599da887dd504b046a82cd8a521e9419a133ee3
Raghav <raghav.gulati@gmail.com>
2021-11-29 18:32:14 +0530

    data changes

--- ws-app/src/app/history/history.component.ts
+++ ws-app/src/app/history/history.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.css']
 })
 export class HistoryComponent implements OnInit {
-  historyList:any=[];
+  historyList:any=[{created:'September 23rd 2021, 10:33 pm',eventNames:'MainUserUpdated, EntityInstanceUpdated',name:'new',byUser:'Christopher Cody Cates'}];
   constructor() { }
 
   ngOnInit(): void {
