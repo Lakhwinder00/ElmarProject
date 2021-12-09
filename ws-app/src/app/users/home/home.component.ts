c5ca14626d165874b2f8fdaf7310747c1a5205f9
Raghav <raghav.gulati@gmail.com>
2021-12-09 20:56:49 +0530

    search feature fixed

--- ws-app/src/app/users/home/home.component.ts
+++ ws-app/src/app/users/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from'@angular/router'
+import { Router } from '@angular/router'
 import { select, Store } from '@ngrx/store';
 import { observable, Observable } from 'rxjs';
 import { IAppState } from '../store/home/app.state'
 
 import { selectAllCourseCatalogSelector, selectCourseCatalogLoading } from '../store/home/app.selector'
 import { courseCatalogList, loadCourseCatalogSuccess } from '../store/home/app.action';
-import {AppHomeEffect } from './../store/home/app.effect';
+import { AppHomeEffect } from './../store/home/app.effect';
 
 @Component({
   selector: 'app-home',
@@ -20,6 +20,11 @@ export class HomeComponent implements OnInit {
   public sponsersettings = {};
   public typesettings = {};
   public selectedItems = [];
+  cradits: any = [];
+  creditSettingValue: any = [];
+  tagsvalue: any = [];
+  typevalue: any = [];
+  sponsersvalue: any;
   tags: any = [];
   credits: any = [];
   sponser: any = [];
@@ -29,12 +34,14 @@ export class HomeComponent implements OnInit {
   tab1: any;
   tab2: any;
   tab3: any;
-  constructor(private router: Router, private store: Store<IAppState>, private homeAffect: AppHomeEffect) { }
+  searchValue: string = "";
+  constructor(private router: Router, private store: Store<IAppState>) { }
   $loading: Observable<boolean> = this.store.select(selectCourseCatalogLoading)
   ngOnInit(): void {
     this.tags = [];
     this.credits = [];
     this.sponser = [];
+
     this.type = [];
     this.store.dispatch(courseCatalogList())
     this.store.select(selectAllCourseCatalogSelector).subscribe((res) => { this.courseCatalog = res });
@@ -122,17 +129,54 @@ export class HomeComponent implements OnInit {
       showSelectedItemsAtTop: false,
       defaultOpen: false,
     };
+
   }
   public onFilterChange(item: any) {
     debugger
     console.log(item);
+    this.commonfilter()
   }
   public onDropDownClose(item: any) {
     console.log(item);
   }
 
-  public onItemSelect(item: any) {
+  public onItemSelect(item: any, code: number) {
     console.log(item);
+    switch (code) {
+      case 1: {
+      this.store.select(selectAllCourseCatalogSelector).subscribe((res) => {
+        this.courseCatalog = res.filter(f => this.creditSettingValue?.some(x => x.item_text?.toLowerCase().includes(f.creditstype.toLowerCase()))
+        )});
+
+        break;
+      }
+      case 2: {
+        this.store.select(selectAllCourseCatalogSelector).subscribe((res) => {
+          this.courseCatalog = res.filter(f => this.tagsvalue?.some(x => x.item_text?.toLowerCase().includes(f.tag.toLowerCase()))
+          )});
+        break;
+      }
+      case 3: {
+        this.store.select(selectAllCourseCatalogSelector).subscribe((res) => {
+          this.courseCatalog = res.filter((f:any) => this.sponsersvalue?.some(x => x.item_text?.toLowerCase().includes(f?.sponser.toLowerCase()))
+          )}); 
+        break;
+      }
+      case 4: {
+        this.store.select(selectAllCourseCatalogSelector).subscribe((res) => {
+          this.courseCatalog = res.filter((f:any) => this.typevalue?.some(x => x.item_text?.toLowerCase().includes(f?.type.toLowerCase()))
+          )}); 
+        break;
+      }
+
+      default: {
+        //statements; 
+        break;
+      }
+    }
+
+    // this.commonfilter(this.searchValue, this.creditSettingValue, this.sponsersvalue, this.tagsvalue, this.typevalue);
+
   }
   public onDeSelect(item: any) {
     debugger
@@ -149,14 +193,34 @@ export class HomeComponent implements OnInit {
     this.store.select(selectAllCourseCatalogSelector).subscribe((res) => {
       this.courseCatalog = res.filter((f) => f.name.toLowerCase().includes($filter.target.value.toLowerCase()))
     })
+
+    // this.commonfilter(this.searchValue, this.creditSettingValue, this.sponsersvalue, this.tagsvalue, this.typevalue);
   }
+  commonfilter(search = "", credit: any = [], sponser: any = [], tags: any = [], type: any = []) {
+    debugger;
+    this.store.select(selectAllCourseCatalogSelector).subscribe((res) => {
+
+      // this.courseCatalog = res.filter(f => tags?.some(x => x.item_text?.toLowerCase().includes(f.tag.toLowerCase()))
+      this.courseCatalog = res; 
+      });
+      //  this.courseCatalog= this.courseCatalog.filter((f:any) => f.name.toLowerCase().includes(search.toLowerCase()) || credit?.some(x => x.item_text?.toLowerCase().includes(f.creditsvalue.toLowerCase())) || tags?.some(x => x.item_text?.toLowerCase().incluedes(f.tag.toLowerCase())) || type?.some(x => x.item_text?.toLowerCase().incluedes(f.type.toLowerCase()))|| this.sponsersvalue?.some(x => x.item_text?.toLowerCase().incluedes(f.sponser.toLowerCase())))
+    }
+
   onClick(isCheck: any) {
-    if (isCheck == 1) {
+      if(isCheck == 1) {
       this.tab = 'tab1';
     } else if (isCheck == 2) {
       this.tab = 'tab2';
     } else {
-      this.tab  = 'tab3';
+      this.tab = 'tab3';
     }
+  }
+  resetFilters(){
+    this.creditSettingValue=[];
+    this.searchValue="";
+    this.sponsersvalue=[];
+    this.tagsvalue=[];
+    this.typevalue=[];
+    this.commonfilter();
   }
 }
