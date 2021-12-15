81acf94ae01e795d4db2bbffe4ea157a0f212e92
Raghav <raghav.gulati@gmail.com>
2021-12-15 21:56:50 +0530

    chnages for

--- ws-app/src/app/admin/dashboard/dashboard.component.ts
+++ ws-app/src/app/admin/dashboard/dashboard.component.ts
@@ -12,7 +12,26 @@ export class DashboardComponent implements OnInit {
 
   msgs: any[];
 
-  constructor( ) {
+  employees: any = [{ Id: 1, Name: 'Nilav', Department: '.net', DepartmentId: 1, Address: 'Ahmedabad, Gujarat', Age: 26 },
+  { Id: 2, Name: 'Nilav1', Department: '.net', DepartmentId: 1, Address: 'Surat, Gujarat', Age: 25 },
+  { Id: 3, Name: 'Nilav2', Department: 'PHP', DepartmentId: 4, Address: 'Ahmedabad, Gujarat', Age: 25 },
+  { Id: 4, Name: 'Nilav3', Department: '.net', DepartmentId: 1, Address: 'Ahmedabad, Gujarat', Age: 30 },
+  { Id: 5, Name: 'Nilav4', Department: 'HR', DepartmentId: 2, Address: 'Baroda, Gujarat', Age: 25 },
+  { Id: 6, Name: 'Nilav5', Department: 'HR', DepartmentId: 2, Address: 'Ahmedabad, Gujarat', Age: 28 },
+  { Id: 7, Name: 'Nilav6', Department: 'Admin', DepartmentId: 3, Address: 'Ahmedabad, Gujarat', Age: 26 },
+  { Id: 8, Name: 'Nilav7', Department: '.net', DepartmentId: 1, Address: 'Surat, Gujarat', Age: 25 },
+  { Id: 9, Name: 'Nilav8', Department: '.net', DepartmentId: 1, Address: 'Ahmedabad, Gujarat', Age: 27 },
+  { Id: 10, Name: 'Nilav9', Department: 'Admin', DepartmentId: 3, Address: 'Baroda, Gujarat', Age: 25 },
+  { Id: 11, Name: 'Nilav10', Department: '.net', DepartmentId: 1, Address: 'Ahmedabad, Gujarat', Age: 35 },
+  ];
+  columns = [
+    { field: 'Name', header: 'Name' },
+    { field: 'Department', header: 'Department' },
+    { field: 'Address', header: 'Address' },
+    { field: 'Age', header: 'Age' }
+  ];
+  pageSize: number = 5;
+  constructor() {
     this.barChartData = {
       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
       datasets: [
@@ -28,7 +47,7 @@ export class DashboardComponent implements OnInit {
           borderColor: '#7CB342',
           data: [28, 48, 40, 19, 86, 27, 90]
         }
-        
+
       ]
     }
 
@@ -51,7 +70,7 @@ export class DashboardComponent implements OnInit {
     };
 
     this.msgs = [];
-    
+
     // translate.get("WelcomeMessage").subscribe((text: string) => {
     //   this.msgs.push({ severity: 'success', summary: '', detail: text });
     // });
@@ -59,7 +78,7 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    console.log("employee=>", this.employees)
   }
 
 }
